eba0af774932a68f8a5f51d3fc9e3fc744880dcc
Happiness24 <mafalompho45@gmail.com>
2023-10-13 11:54:10 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,15 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}else
+	}
+	else
 	{
 		int i;
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('_');
+		_putchar('_');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
